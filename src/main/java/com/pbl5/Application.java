f98fd7485b8794f85fbee0d4a616ccbbb05f107f
Bao Nguyen <102230003@sv1.dut.udn.vn>
2026-04-03 11:35:35 +0700

    Khởi tạo môi trường framework Spring Boot MV

---
+++ src/main/java/com/pbl5/Application.java
@@ -0,0 +1,18 @@
+package com.pbl5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        System.out.println("\n");
+        System.out.println("==========================================================");
+        System.out.println("🚀 MÁY CHỦ JAVA SPRING BOOT ĐÃ CHẠY THÀNH CÔNG RỰC RỠ! 🚀");
+        System.out.println("🌐 Địa chỉ nội bộ Mac:  http://localhost:8080");
+        System.out.println("📱 Địa chỉ cho LAN:   http://10.30.212.72:8080");
+        System.out.println("==========================================================\n");
+    }
+}
+
